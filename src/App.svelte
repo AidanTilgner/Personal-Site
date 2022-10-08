4cb91adf4c7042071acea3927f0c115c4170af6d
Aidan Tilgner <aidantilgner02@gmail.com>
2022-10-08 01:24:02 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  export let url: string;
+  import { onMount, onDestroy } from "svelte";
+  import { Router, Route, navigate } from "svelte-routing";
+  import { currentPath } from "@lib/stores/env";
+  import { globalHistory } from "svelte-routing/src/history";
+  import { initSocket } from "@src/bootstrap/socket.io";
+  import { messages } from "@lib/stores/socket";
+  import { dispatchAlert } from "@lib/stores/alerts";
+
+  import AlertProvider from "./lib/components/Alerts/AlertProvider.svelte";
+  import Navbar from "./lib/components/Navbar/Navbar.svelte";
+
+  import Landing from "./pages/Landing/Landing.svelte";
+
+  initSocket();
+
+  let unsubscribe: () => void;
+  onMount(() => {
+    unsubscribe = globalHistory.listen(({ location, action }) => {
+      $currentPath = location.pathname;
+    });
+
+    navigate($currentPath);
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+</script>
+
+<main>
+  <AlertProvider />
+  <Navbar />
+  <Router {url}>
+    <div class="content-container">
+      <Route path="/" component={Landing} />
+    </div>
+  </Router>
+</main>
+
+<style lang="scss">
+  @use "./lib/styles/partials/mixins" as *;
+
+  .content-container {
+    @include default-padding;
+  }
+</style>
---
+++ src/lib/components/Alerts/AlertProvider.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { currentAlert } from "@lib/stores/alerts";
+  import { Alert } from "@src/declarations/main";
+
+  let alert: Alert | null = null;
+  let open: boolean = false;
+  let timeoutState = setTimeout(() => {}, 0);
+  $: if ($currentAlert) {
+    alert = $currentAlert;
+    const { visible, timeout } = $currentAlert;
+    if (visible) {
+      open = true;
+    }
+    if (timeout) {
+      clearTimeout(timeoutState);
+      timeoutState = setTimeout(() => {
+        open = false;
+        alert = null;
+      }, timeout);
+    }
+  }
+</script>
+
+{#if alert}
+  <div class="container">alert</div>
+{/if}
+
+<style lang="scss">
+  .container {
+    position: fixed;
+    top: 14px;
+    left: 24px;
+    right: 24px;
+    z-index: 1000;
+
+    animation-name: dropin;
+    animation-duration: 0.6s;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
+  }
+
+  @keyframes dropin {
+    from {
+      transform: translateY(-75%);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+</style>
