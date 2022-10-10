f79e46a80dc404545280ac5e635001a47e83a827
Aidan Tilgner <aidantilgner02@gmail.com>
2022-10-09 23:45:47 -0700

    added scss and some other styles

--- src/components/Buttons/Primary.svelte
+++ src/components/Buttons/Primary.svelte
@@ -2,7 +2,7 @@
     export let text: string = "";
 </script>
 
-<button>
+<button class="background-gradient">
     {#if text}
         {text}
     {:else}
@@ -10,9 +10,8 @@
     {/if}
 </button>
 
-<style>
+<style lang="scss">
     button {
-        background-color: #3D6EFF;
         color: #fff;
         box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.15);
         border-radius: 99px;
@@ -23,6 +22,7 @@
         font-family: "Quicksand", sans-serif;
         cursor: pointer;
         transition: all 0.2s ease-in-out;
+        font-weight: 500;
     }
 
     button:hover {
@@ -34,6 +34,6 @@
     button:active {
         background-color: #1A3DA1;
         box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.35);
-        transform: scale(0.98);
+        transform: scale(0.98) translateY(2px);
     }
 </style>
---
+++ src/components/Buttons/Secondary.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    export let text: string = "";
+</script>
+
+<button>
+    <span>
+        {#if text}
+            {text}
+        {:else}
+            <slot />
+        {/if}
+    </span>
+    <i class="material-icons icon text-gradient">keyboard_arrow_right</i>
+</button>
+
+<style lang="scss">
+    @use "../../styles/mixins" as *;
+
+    button {
+        background-color: transparent;
+        color: #000;
+        /* box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.15); */
+        border-radius: 99px;
+        border: none;
+        width: 150px;
+        padding: 10px 0;
+        font-size: 18px;
+        font-family: "Quicksand", sans-serif;
+        cursor: pointer;
+        transition: all 0.2s ease-in-out;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 500;
+        transition: all 0.2s ease-in-out;
+    }
+
+    button:hover {
+        /* The icon should move to the right */
+        .icon {
+            transition: all 0.2s ease-in-out;
+            transform: translateX(8px);
+        }
+        span {
+            @include text-gradient;
+        }
+    }
+
+    .icon {
+        font-size: 22px;
+        // color: #3D6EFF;
+        margin-left: 8px;
+    }
+
+    button:active {
+        .icon {
+            transition: all 0.1s ease-in-out;
+            transform: translateX(16px) scale(0.5);
+            opacity: 0;
+            margin: 0;
+        }
+
+        span {
+            transition: all 0.1s ease-in-out;
+            transform: scale(0.95) translateX(8px);
+        }
+    }
+</style>
