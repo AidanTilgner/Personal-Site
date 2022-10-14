9ca3dae4abb60a032e6ace92e674cdc8eb305e68
Aidan Tilgner <aidantilgner02@gmail.com>
2022-10-14 00:13:45 -0700

    added netlify stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   output: "server",
-  adapter: node()
+  adapter: netlify()
 });
