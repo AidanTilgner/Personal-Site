9e061911fb10bec10ea36f35c145992e31c48d78
Aidan Tilgner <aidantilgner02@gmail.com>
2022-12-22 23:01:18 -0800

    added tutoring to footer

--- src/components/Navigation/Nav.svelte
+++ src/components/Navigation/Nav.svelte
@@ -93,6 +93,7 @@
         <a href="/">Home</a>
         <a href="/skills">Skills</a>
         <a href="/projects">Projects</a>
+        <a href="/blog">Blog</a>
         <a href="/contact">Contact</a>
       </div>
       <div class="buttons">
