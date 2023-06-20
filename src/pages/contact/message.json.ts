9a4cf31e4d78ac9e9b37e4009a1962363d6d5598
atilgner <aidantilgner02@gmail.com>
2023-06-20 01:14:32 -0700

    feat: response message

--- src/pages/contact/message.json.ts
+++ src/pages/contact/message.json.ts
@@ -14,10 +14,27 @@ export const post: APIRoute = async ({ request }) => {
       text: message,
       html: `
         <i>This message is from <a href="https://aidantilgner.dev" target="_blank">aidantilgner.dev</a></i>
-        <hr style="border-coler" />
+        <hr />
         <br />
         <p>
           ${message}
+        </p>
+        <br />
+        <hr />
+        <p>Reply to ${name} at <a href="mailto:${email}">${email}</a></p>
+      `,
+    });
+
+    sendEmail({
+      to: email,
+      subject: "Thanks for reaching out!",
+      text: "Thanks for reaching out!",
+      html: `
+        <i>This message is from <a href="https://aidantilgner.dev" target="_blank">aidantilgner.dev</a></i>
+        <hr />
+        <br />
+        <p>
+        Thanks for reaching out! I'll get back to you as soon as I can.
         </p>
         <br />
         <hr />
