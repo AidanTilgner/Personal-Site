cd1cbf1ba158cb5c4530bf2896bc92349142a29a
atilgner <aidantilgner02@gmail.com>
2023-08-08 08:18:07 -0700

    feat: blocks on server url

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,12 @@ import Express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import contentRouter from "./routes/content";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 config();
 
@@ -12,6 +18,7 @@ const app = Express();
 app.use(cors());
 
 app.use("/content", contentRouter);
+app.use(Express.static(path.join(__dirname, "./public")));
 
 app.listen(PORT, () => {
   console.info(`Server listening on port ${PORT}`);
