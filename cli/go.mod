e05cb3e47cded9424ca5f90f233720d3416bacd0
atilgner <aidantilgner02@gmail.com>
2023-08-08 21:03:05 -0700

    feat: init better default cli, nothing here yet

--- cli/go.mod
+++ cli/go.mod
@@ -1,6 +1,6 @@
-module personal-site-cli
+module github.com/charmbracelet/bubbletea-app-template
 
-go 1.20
+go 1.19
 
 require (
 	github.com/charmbracelet/bubbles v0.16.1
@@ -9,7 +9,6 @@ require (
 )
 
 require (
-	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
--- cli/main.go
+++ cli/main.go
@@ -2,140 +2,74 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	comps "personal-site-cli/components"
 
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-type Styles struct {
-	BorderColor lipgloss.Color
-	InputField  lipgloss.Style
+type errMsg error
+
+type model struct {
+	spinner  spinner.Model
+	quitting bool
+	err      error
 }
 
-func DefaultStyles() *Styles {
-	s := new(Styles)
-	s.BorderColor = lipgloss.Color("36")
-	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
-	return s
+var quitKeys = key.NewBinding(
+	key.WithKeys("q", "esc", "ctrl+c"),
+	key.WithHelp("", "press q to quit"),
+)
+
+func initialModel() model {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	return model{spinner: s}
 }
 
-type Main struct {
-	styles    *Styles
-	index     int
-	questions []Question
-	width     int
-	height    int
-	done      bool
+func (m model) Init() tea.Cmd {
+	return m.spinner.Tick
 }
 
-type Question struct {
-	question string
-	answer   string
-	input    comps.Input
-}
-
-func newQuestion(q string) Question {
-	return Question{question: q}
-}
-
-func newShortQuestion(q string) Question {
-	question := newQuestion(q)
-	model := comps.NewShortAnswerField()
-	question.input = model
-	return question
-}
-
-func newLongQuestion(q string) Question {
-	question := newQuestion(q)
-	model := comps.NewLongAnswerField()
-	question.input = model
-	return question
-}
-
-func New(questions []Question) *Main {
-	styles := DefaultStyles()
-	return &Main{styles: styles, questions: questions}
-}
-
-func (m Main) Init() tea.Cmd {
-	return m.questions[m.index].input.Blink
-}
-
-func (m Main) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	current := &m.questions[m.index]
-	var cmd tea.Cmd
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
+
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		if key.Matches(msg, quitKeys) {
+			m.quitting = true
 			return m, tea.Quit
-		case "enter":
-			if m.index == len(m.questions)-1 {
-				m.done = true
-			}
-			current.answer = current.input.Value()
-			m.Next()
-			return m, current.input.Blur
+
 		}
+		return m, nil
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
 	}
-	current.input, cmd = current.input.Update(msg)
-	return m, cmd
 }
 
-func (m Main) View() string {
-	current := m.questions[m.index]
-	if m.done {
-		var output string
-		for _, q := range m.questions {
-			output += fmt.Sprintf("%s: %s\n", q.question, q.answer)
-		}
-		return output
+func (m model) View() string {
+	if m.err != nil {
+		return m.err.Error()
 	}
-	if m.width == 0 {
-		return "loading..."
+	str := fmt.Sprintf("\n\n   %s Loading forever... %s\n\n", m.spinner.View(), quitKeys.Help().Desc)
+	if m.quitting {
+		return str + "\n"
 	}
-	// stack some left-aligned strings together in the center of the window
-	return lipgloss.Place(
-		m.width,
-		m.height,
-		lipgloss.Center,
-		lipgloss.Center,
-		lipgloss.JoinVertical(
-			lipgloss.Left,
-			current.question,
-			m.styles.InputField.Render(current.input.View()),
-		),
-	)
-}
-
-func (m *Main) Next() {
-	if m.index < len(m.questions)-1 {
-		m.index++
-	} else {
-		m.index = 0
-	}
+	return str
 }
 
 func main() {
-	// init styles; optional, just showing as a way to organize styles
-	// start bubble tea and init first model
-	questions := []Question{newShortQuestion("what is your name?"), newShortQuestion("what is your favourite editor?"), newLongQuestion("what's your favourite quote?")}
-	main := New(questions)
-
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		fmt.Println("fatal:", err)
-		os.Exit(1)
-	}
-	defer f.Close()
-	p := tea.NewProgram(*main, tea.WithAltScreen())
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
